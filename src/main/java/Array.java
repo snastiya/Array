052ab3c98042d8916d1c21059f7f6a1fdc337b94
Anastasiya Sudarenko <snastiya@mail.ru>
2017-10-16 20:48:37 +0300

    new

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -14,7 +14,6 @@ public class Array {
 
         Scanner in = new Scanner(System.in);
 
-
         for (String element : anArr) {
 
             System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
@@ -23,21 +22,18 @@ public class Array {
             i++;
         }
 
-        int index = 0;
         int max = 0;
-        String maxval  = null;
+        String maxval = null;
 
         for (String element : anArr) {
-            if (element.length() > max)
-            {
+            if (element.length() > max) {
                 max = element.length();
                 maxval = element;
             }
         }
 
-            //System.out.print(element + " ");
         System.out.println("Самое длинное слово в массиве имеет номер: " + maxval);
 
-            }
+    }
 
 }
